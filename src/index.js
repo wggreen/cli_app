7cd2fdc5e97d651108d3041a7a5d3b1d06a4dfdb
max beizer <max.beizer@gmail.com>
2021-07-02 11:39:55 -0500

    Begin work on printing a test string
    Co-authored-by: William Green <wggreen.nss@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const {Command, flags} = require('@oclif/command')
 
+/**
+ * name of co, date of upcoming, name of contact, website[
+*/
 class CliAppCommand extends Command {
   async run() {
     const {flags} = this.parse(CliAppCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.js`)
+    const csv = `
+    AcmeCo,07/02/21,Donald Duck,disney.com,donald@disney.com,
+    GitHub,07/03/21,Nat Fridman,github.com,nat@github.com,
+    `
+    const lines = csv.split("\n")
+
+    const header = `Upcoming interviews\n`
+    this.log(header)
+
+    const tableHeader = `Name   | Date   | Email   | Website   \n`
+    this.log(tableHeader)
+
+    lines.forEach(line => {
+      if (line !== "") {
+        this.log(line.trim())
+      }
+    })
   }
 }
 
